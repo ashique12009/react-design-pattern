665de5d879beb2d1952a2d0d1057d4d3c83b7161
Ashique <ashique12009@gmail.com>
2026-04-12 10:43:38 +0300

    axios and tailwindcss package install, messy component user profile added

--- container-presenter-pattern/src/App.jsx
+++ container-presenter-pattern/src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
+import './App.css'
+import UserProfile from './UserProfile'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <UserProfile userId={1} />
     </>
   )
 }
