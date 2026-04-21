f4403f92c778763686ea26a86c2f645f5761f4e5
Ashique <ashique12009@gmail.com>
2026-04-21 09:35:48 +0300

    with compound pattern

--- compound-pattern/src/App.jsx
+++ compound-pattern/src/App.jsx
@@ -3,18 +3,29 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import Modal from './messy/Modal'
+// import Modal from './messy/Modal'
+import Modal from './with-pattern/modal/Modal'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isOpen, setIsOpen] = useState(false)
 
   return (
     <>
-      <Modal title="Welcome to the Modal"
+      <button onClick={() => setIsOpen(true)}>Open Modal</button>
+      {/* <Modal title="Welcome to the Modal"
         body="This is a simple modal component. It can be used to display important information or to get user input."
         primaryAction={<button className="primary-btn">Primary Action</button>}
         secondaryAction={<button className="secondary-btn">Secondary Action</button>}
-      />
+      /> */}
+
+      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        <Modal.Header>Welcome to the Modal</Modal.Header>
+        <Modal.Body>This is a simple modal component. It can be used to display important information or to get user input.</Modal.Body>
+        <Modal.Footer>
+          <button className="primary-btn">Primary Action</button>
+          <button className="secondary-btn">Secondary Action</button>
+        </Modal.Footer>
+      </Modal>
     </>
   )
 }
